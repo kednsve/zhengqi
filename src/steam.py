df0a3e19143cf9d9e56f4f01cfe2a13a5f9b0368
kedns <kednsve@163.com>
2026-04-19 12:02:12 +0800

    class Steam

--- src/steam.py
+++ src/steam.py
@@ -6,25 +6,43 @@ from utils import Logger
 
 
 class Steam:
-    def __init__(self, data, estimator, log_name):
-        self.logger = Logger(log_name).get_logger()
+    def __init__(self, data, log_name, estimator=None):
+        self.logger = self._get_logger(log_name)
         self.data = self._get_data(data)
-        self.estimator = self._get_estimator(estimator + ".pkl")
+        self.estimator = self._get_estimator(estimator)
+
+    @staticmethod
+    def _get_logger(log_name):
+        logger = Logger(log_name).get_logger()
+        logger.info("logger online")
+        return logger
 
     def _get_data(self, data):
         try:
             df = pd.read_csv(config[data], sep="\t")
+            self.logger.info("data load " + data)
         except Exception as e:
+            self.logger.error("data loading error")
+            self.logger.error("=" * 30)
             self.logger.error(e)
+            self.logger.error("=" * 30)
             exit()
         else:
             return df
 
     def _get_estimator(self, estimator_name):
+        if estimator_name is None:
+            self.logger.info("estimator is None")
+            return None
         try:
-            estimator = joblib.load(config["model_path"] / estimator_name)
+            file = estimator_name + ".pkl"
+            estimator = joblib.load(config["model_path"] / file)
+            self.logger.info("estimator load " + file)
         except Exception as e:
+            self.logger.error("estimator load error")
+            self.logger.error("=" * 30)
             self.logger.error(e)
+            self.logger.error("=" * 30)
             exit()
         else:
             return estimator
