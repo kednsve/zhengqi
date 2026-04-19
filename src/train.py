531bef725359c0d140f2203958ad5da9ae8f59ab
kedns <kednsve@163.com>
2026-04-19 23:05:37 +0800

    try GradientBoostRegressor

--- src/train.py
+++ src/train.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
@@ -18,6 +18,20 @@ def train(steam: Steam):
     steam.estimator = do_gridsearch(
         steam.estimator, params, x_train, y_train, steam.logger
     )
+    # max_depth = 20  n_estimators = 175
     steam.logger.info("predict")
     y_pre = steam.estimator.predict(x_test)
+    steam.logger.info(f"MSE: {mean_squared_error(y_test, y_pre)}")
+    steam.logger.info("try GradientBoostingRegressor")
+    gb = GradientBoostingRegressor()
+    params = {
+        "n_estimators": [125, 150, 175],
+        "max_depth": [15, 20, 25],
+        "learning_rate": [0.1, 0.07, 0.15],
+    }
+    gb: GradientBoostingRegressor = do_gridsearch(
+        gb, params, x_train, y_train, steam.logger
+    )
+    steam.logger.info("predict")
+    y_pre = gb.predict(x_test)
     steam.logger.info(f"MSE: {mean_squared_error(y_test, y_pre)}")
