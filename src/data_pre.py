53acf89a4d844ee89b33b08e07ae2ed280115ed0
kedns <kednsve@163.com>
2026-04-21 20:16:29 +0800

    predict

--- src/data_pre.py
+++ src/data_pre.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from config import config
 from .steam import Steam
 
 
@@ -5,6 +8,8 @@ def data_pre(steam: Steam):
     steam.logger.info("get best model")
     steam.set_best_model()
     steam.logger.info("predict")
-    print(steam.estimator)
-    # y_pre=steam.estimator.predict(steam.data)
-    # print(y_pre)
+    y_pre = steam.estimator.predict(steam.data)
+    steam.logger.info(f"y_pre={y_pre}")
+    res = np.array(y_pre).reshape(-1, 1)
+    np.savetxt(config["data_path"] / "zhengqi_test_pre.txt", res, fmt="%.3f")
+    steam.logger.info("save result")
